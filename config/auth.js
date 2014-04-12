1912883096e2ca6e616c75e4ada1c4f3d2af4cd9
theptrk <patrick.tran06@gmail.com>
2014-04-12 11:54:53 -0700

    initial all

---
+++ config/auth.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '407795085979356',
+		'clientSecret' 	: '85a9e353f5e2d36d26924516f1707959',
+		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+	}, // ???
+
+	'twitterAuth' : {
+		'consumerKey'  	: 'VrELXYKRoFD4hhZAkJTig',
+		'consumerSecret': 'RXRK0KipKd6UTH5SYfaVPBeLTuIQjr0pSVvPsaOig',
+		'callbackURL' 	: 'http://127.0.0.1:8080/auth/twitter/callback'
+		//patrick.tran06@gmail.com
+	},
+
+	'googleAuth' : {
+		'clientID' 		: '396674095780-plnn9uvvegu47pdgdq0i42rko7olq48t.apps.googleusercontent.com',
+		'clientSecret' 	: '-Nu8KIusbjhuJvLDcoYvsGnZ',
+		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+	},
+
+	'linkedinAuth' : {
+		'apiKey'			: '77p5axhp111kjd' ,
+		'secretKey'			: 'HYylYWe6lrYh92CJ' ,
+		'OAuth User Token' 	: '7c99aa5d-d2e2-4460 -b90f-594cf6bbf0ee' ,
+		'OAuth User Secret' : 'dfffdf58-5dd7-43e5-8394-44b3aec99677' ,
+		'callbackURL'		: 'http://127.0.0.1:8080/auth/linkedin/callback'
+	}
+
+
+};
+
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'url'	: 'mongodb://theptrk:kale@troup.mongohq.com:10065/fieldtest'
+	//'url' :  'mongodb://theptrk:kale@troup.mongohq.com:10018/ScotchSocial'
+	//'url' : 'mongodb://theptrk:kale@novus.modulusmongo.net:27017/ahYsaw6o'
+}
